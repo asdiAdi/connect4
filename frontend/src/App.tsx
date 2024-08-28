f6580174c08bba3d819fd58a22d82c24459b0d72
asdiAdi <carltimothyadi@gmail.com>
2024-08-28 22:59:36 +0800

    frontend-init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+function App() {
+  return (
+    <div>
+      <div className={styles["test"]}>This is a test</div>
+    </div>
+  );
+}
+
+export default App;
