5bd59847260a7f0b96d6b2ef852cd695b006c09a
asdiAdi <carltimothyadi@gmail.com>
2024-09-06 12:30:46 +0800

    added posgres database

--- backend/index.ts
+++ backend/index.ts
@@ -5,15 +5,41 @@ import { createServer } from "node:http";
 // import { rooms } from "./src/controllers/controller";
 // import { hasPlayer1, hasPlayer2, hasRoomId } from "./src/utils/room";
 import { route } from "./src/routes/route";
+import { Sequelize } from "sequelize";
 
 const PORT = process.env.PORT;
 // const API = `http://localhost:${PORT}`;
+const PGHOST = process.env.PGHOST as string;
+const PGPORT = process.env.PGPORT as string;
+const PGUSER = process.env.PGUSER as string;
+const PGPASSWORD = process.env.PGPASSWORD as string;
+const PGDATABASE = process.env.PGDATABASE as string;
+
+console.log(PGHOST, PGPORT, PGUSER, PGPASSWORD, PGDATABASE);
 
 const app = express();
 // parse application/json
 app.use(express.json({ limit: "10mb" }));
 
 const server = createServer(app);
+const sequelize = new Sequelize(
+  `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`,
+);
+// const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+//   dialect: "postgres",
+//   dialectOptions: {
+//   },
+// });
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
 // const io = new Server(server, {
 //   cors: { origin: true },
 // });
